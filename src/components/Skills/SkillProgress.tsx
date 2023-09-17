ad3f9eaa09ba5f1b9e06ba1339ff2b2818dbbc83
Oleksandr Aleksieiev <frontdev1991@gmail.com>
2023-09-17 21:27:59 +0200

    add animation count skill value package

--- src/components/Skills/SkillProgress.tsx
+++ src/components/Skills/SkillProgress.tsx
@@ -1,11 +1,17 @@
-import React, {FC, ReactElement} from 'react';
+'use client';
+
+import React, {FC, ReactElement, useState} from 'react';
 import {ISkill} from 'my-portfolio-types';
+import CountUp from 'react-countup';
+import VisibilitySensor from 'react-visibility-sensor';
 
 interface Props {
   skill: ISkill;
 }
 
 const SkillProgress: FC<Props> = ({skill}): ReactElement => {
+  const [focus, setFocus] = useState(false);
+
   return (
     <div className={'w-full mb-6'}>
       <h6 className={'text-lg mb-4 font-bold w-full whitespace-nowrap text-ellipsis overflow-hidden'}>
@@ -16,7 +22,26 @@ const SkillProgress: FC<Props> = ({skill}): ReactElement => {
         <div
           className={'text-xs font-medium text-center p-1 leading-none rounded-full'}
           style={{ width: `${skill.experience}%`, backgroundColor: skill.color }}>
-          {skill.experience} %
+          <span>
+            <CountUp
+              start={focus ? 0 : 0}
+              end={Number(skill.experience)}
+              duration={1}
+              redraw={true}>
+              {({ countUpRef }) => (
+                <VisibilitySensor
+                  onChange={(isVisible) => {
+                    if (isVisible) {
+                      setFocus(true);
+                    }
+                  }}>
+
+                  <span ref={countUpRef}/>
+                </VisibilitySensor>
+              )}
+            </CountUp>
+            %
+          </span>
         </div>
       </div>
     </div>
