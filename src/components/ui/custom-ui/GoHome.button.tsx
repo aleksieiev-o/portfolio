3c33718f369a7411ffa603d724df03dd9b185f5b
Oleksandr Aleksieiev <frontdev1991@gmail.com>
2023-11-23 18:15:34 +0100

    changed not found page

--- src/components/ui/custom-ui/GoHome.button.tsx
+++ src/components/ui/custom-ui/GoHome.button.tsx
@@ -4,12 +4,15 @@ import React, {FC, ReactElement} from 'react';
 import {Button} from '@/components/ui/button';
 import {RoutePath} from '@/router/Routes.enum';
 import {useRouter} from 'next/navigation';
+import {Home} from 'lucide-react';
 
 const GoHomeButton: FC = (): ReactElement => {
   const router = useRouter();
 
   return (
-    <Button onClick={() => router.replace(RoutePath.HOME)}>
+    <Button onClick={() => router.replace(RoutePath.HOME)} variant={'outline'} className={'shadow-md'} title={'Go home'}>
+      <Home className={'w-4 h-4 mr-4'}/>
+
       Go home
     </Button>
   );
