6d800367bf0676ff714bfb68d51260bd97802be9
Oleksandr Aleksieiev <frontdev1991@gmail.com>
2023-11-25 23:16:29 +0100

    added logo and favicon

--- src/components/AppHeader.tsx
+++ src/components/AppHeader.tsx
@@ -2,15 +2,18 @@ import { RoutePath } from '@/router/Routes.enum';
 import Link from 'next/link';
 import React, {FC, ReactElement} from 'react';
 import AppThemeChange from '@/components/ui/custom-ui/AppThemeChange';
+import LogoIcon from '@/components/Logo.icon';
 
 const AppHeader: FC = (): ReactElement => {
   return (
-    <header className={'w-full h-20 flex flex-row items-center justify-between overflow-hidden border-b shadow-md'}>
-      <Link href={RoutePath.HOME} className={'w-12 md:w-20 h-12 md:h-20 mr-4 flex items-center justify-center'}>
-        OA
-      </Link>
+    <header className={'w-full h-20 flex flex-row items-center justify-between overflow-hidden border-b shadow-md pl-2 md:pl-4 pr-4 md:pr-8'}>
+      <div className={'h-full'}>
+        <Link href={RoutePath.HOME} className={'w-full h-full mr-4 flex items-center justify-center'}>
+          <LogoIcon height={'3rem'}/>
+        </Link>
+      </div>
 
-      <div className={'h-20 grid grid-flow-col auto-cols-max gap-4 md:gap-8 items-center pr-4 md:pr-8'}>
+      <div className={'h-20 grid grid-flow-col auto-cols-max gap-4 md:gap-8 items-center'}>
         <AppThemeChange/>
 
         {/* TODO add localization */}
